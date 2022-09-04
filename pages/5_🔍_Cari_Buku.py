26936f51ba1d0344d6719c6c37e170315d6bcf8c
Abdullah Fikri <abdullahfikrihandi@gmail.com>
2022-09-04 19:59:38 +0700

    ℹ️ extract method & show available stock

--- pages/5_🔍_Cari_Buku.py
+++ pages/5_🔍_Cari_Buku.py
@@ -30,54 +30,45 @@ pencarian.sort()
 
 options = st.multiselect(f'Cari berdasarkan {based_on}', pencarian)
 
+
+def show_cari_buku(filter):
+    st.write(f'- Judul = {data_buku[x]["judul"]}')
+    st.write(f'- ID Buku = {x}')
+    if filter != 'penulis':
+        st.write(f'- Penulis = {data_buku[x]["penulis"]}')
+    if filter != 'penerbit':
+        st.write(f'- Penerbit = {data_buku[x]["penerbit"]}')
+    st.write(f'- Kuantitas = {data_buku[x]["kuantitas"]}')
+    st.write(f'- Stock = {buku_stock(x)}')
+    if filter != 'kategori':
+        st.write(f'- Kategori = {data_buku[x]["kategori"]}')
+    st.write("***")
+
+
 if based_on == 'Penulis':
     for i in options:
         st.subheader(f'Penulis = {i}')
         for x in data_buku:
             if data_buku[x]["penulis"] == i:
-                st.write(f'- Judul = {data_buku[x]["judul"]}')
-                st.write(f'- ID Buku = {x}')
-                st.write(f'- Penerbit = {data_buku[x]["penerbit"]}')
-                st.write(f'- Kuantitas = {data_buku[x]["kuantitas"]}')
-                st.write(f'- Stock = {buku_stock(x)}')
-                st.write(f'- Kategori = {data_buku[x]["kategori"]}')
-                st.write("***")
+                show_cari_buku('penulis')
 
 elif based_on == 'Judul Buku':
     for i in options:
         st.subheader(i)
         for x in data_buku:
             if data_buku[x]["judul"] == i:
-                st.write(f'- Judul = {data_buku[x]["judul"]}')
-                st.write(f'- ID Buku = {x}')
-                st.write(f'- Penulis = {data_buku[x]["penulis"]}')
-                st.write(f'- Penerbit = {data_buku[x]["penerbit"]}')
-                st.write(f'- Kuantitas = {data_buku[x]["kuantitas"]}')
-                st.write(f'- Kategori = {data_buku[x]["kategori"]}')
-                st.write("***")
+                show_cari_buku('')
 
 elif based_on == 'Penerbit':
     for i in options:
         st.subheader(f'Penerbit = {i}')
         for x in data_buku:
             if data_buku[x]["penerbit"] == i:
-                # st.write("***")
-                st.write(f'- Judul = {data_buku[x]["judul"]}')
-                st.write(f'- ID Buku = {x}')
-                st.write(f'- Penulis = {data_buku[x]["penulis"]}')
-                st.write(f'- Kuantitas = {data_buku[x]["kuantitas"]}')
-                st.write(f'- Kategori = {data_buku[x]["kategori"]}')
-                st.write("***")
+                show_cari_buku('penerbit')
 
 elif based_on == 'Kategori':
     for i in options:
         st.subheader(f'Kategori = {i}')
         for x in data_buku:
             if data_buku[x]["kategori"] == i:
-                # st.write("***")
-                st.write(f'- Judul = {data_buku[x]["judul"]}')
-                st.write(f'- ID Buku = {x}')
-                st.write(f'- Penulis = {data_buku[x]["penulis"]}')
-                st.write(f'- Penerbit = {data_buku[x]["penerbit"]}')
-                st.write(f'- Kuantitas = {data_buku[x]["kuantitas"]}')
-                st.write("***")
+                show_cari_buku('kategori')
